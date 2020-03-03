b301c93f2a6a961d674c99e71b4cd2ddc2dca083
Nikolas Haug <nikohaug21@gmail.com>
2020-03-03 13:26:07 -0600

    add custom page and post templates

---
+++ attachment.php
@@ -0,0 +1,63 @@
+<?php get_header();?>
+
+    <!-- Content
+    ============================================= -->
+    <section id="content">
+
+        <div class="content-wrap">
+
+            <div class="container clearfix">
+
+                <!-- Post Content
+          ============================================= -->
+                <div class="postcontent nobottommargin clearfix">
+
+                    <?php if (have_posts()): ?>
+                        <?php while (have_posts()): the_post();?>
+
+	                            <div class="single-post nobottommargin">
+
+
+	                                <!-- Single Post
+	                            ============================================= -->
+	                                <div class="entry clearfix">
+
+	                                    <!-- Entry Title
+	                                ============================================= -->
+	                                    <div class="entry-title">
+	                                        <h2><?php the_title();?></h2>
+	                                    </div><!-- .entry-title end -->
+
+                                    <!-- Entry Content
+                                ============================================= -->
+                                    <div class="entry-content notopmargin">
+
+                                        <a href="<?php echo $post->guid; ?>">Direct Download</a>
+                                        <?php the_content( ); ?>
+                                        <!-- Content End -->
+                                    <div class="clear"></div>
+
+                                <?php 
+                                
+                                    if(comments_open() || get_comments_number( )) {
+                                        comments_template(); 
+                                    }
+                                    
+                                ?>
+
+                            </div>
+
+                            <?php endwhile;?>
+                                <?php endif;?>
+
+                                </div><!-- .postcontent end -->
+
+                                <?php get_sidebar();?>
+
+                                </div>
+
+                            </div>
+
+                        </section><!-- #content end -->
+
+                        <?php get_footer();?>
---
+++ full-width.php
@@ -0,0 +1,92 @@
+<?php 
+/**
+ * Template Name: Full Width Page
+ */
+?>
+
+<?php get_header();?>
+
+    <section id="page-title">
+        <div class="container clearfix">
+            <h1><?php single_post_title(); ?></h1>
+            <span>
+                <?php if(function_exists('the_subtitle')) : ?>
+                    <?php the_subtitle(); ?>
+                <?php endif; ?>
+            </span>
+        </div>
+    </section><!-- #page-title end -->
+
+    <!-- Content
+    ============================================= -->
+    <section id="content">
+
+        <div class="content-wrap">
+
+            <div class="container clearfix">
+
+                <!-- Post Content
+          ============================================= -->
+
+                        <?php while (have_posts()): the_post();?>
+
+		                            <?php
+
+                                    global $post;
+                                    $author_ID = $post->post_author;
+                                    $author_URL = get_author_posts_url($author_ID);
+
+                                    ?>
+
+		                            <div class="single-post nobottommargin">
+
+		                                <!-- Single Post
+		                            ============================================= -->
+		                                <div class="entry clearfix">
+
+		                                    <!-- Entry Image
+                                            ============================================= -->
+                                            <?php if (has_post_thumbnail()): ?>
+                                                <div class="entry-image">
+                                                    <a href="<?php the_permalink();?>">
+                                                        <?php the_post_thumbnail('full');?>
+                                                    </a>
+                                                </div>
+                                            <?php endif;?><!-- .entry-image end -->
+
+                                    <!-- Entry Content
+                                ============================================= -->
+                                        <div class="entry-content notopmargin">
+
+                                        <?php the_content();?>
+                                        <?php
+
+                                            $defaults = array(
+                                                'before' => '<p class="text-center">' . __('Pages:', 'wp_dev_theme'),
+                                                'after' => '</p>',
+                                            );
+                                            wp_link_pages($defaults);
+
+                                            ?>
+                                    </div><!-- Post Single - Content End -->
+                                </div><!-- .entry end -->
+
+                                </div>
+
+                                <?php
+
+                                if (comments_open() || get_comments_number()) {
+                                    comments_template();
+                                }
+
+                                ?>
+
+                            <?php endwhile;?>
+
+                               
+
+                            </div>
+
+                        </section><!-- #content end -->
+
+                        <?php get_footer();?>
