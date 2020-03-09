c6883997f0e53e027f188b4f1ae08ccb86dfa0ac
Nikolas Haug <nikohaug21@gmail.com>
2020-03-09 14:51:48 -0500

    add test page for template tags

---
+++ experimental.php
@@ -0,0 +1,35 @@
+<?php 
+/**
+ * Template Name: Experimental
+ */
+?>
+
+<?php get_header();?>
+
+    <section id="page-title">
+        <div class="container clearfix">
+            <h1>Experimental Code</h1>
+        </div>
+    </section><!-- #page-title end -->
+
+    <!-- Content
+    ============================================= -->
+    <section id="content">
+
+        <div class="content-wrap">
+
+            <div class="container clearfix">
+
+                <!-- Post Content
+          ============================================= -->
+
+                <?php single_post_title(); ?>
+                               
+
+            </div>
+        
+        </div>
+
+    </section><!-- #content end -->
+
+    <?php get_footer();?>
